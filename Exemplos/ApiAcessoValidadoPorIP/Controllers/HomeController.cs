1ad954dd5bec55adad3dda462a8d330353401096
janderson-bkcode <janderson.goncalves@bkbank.com.br>
2022-10-26 04:20:14 -0300

    update

---
+++ Exemplos/ApiAcessoValidadoPorIP/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using ApiAcessoValidadoPorIP.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAcessoValidadoPorIP.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        
+        [HttpGet("Unblocked")]
+        public string Unblocked()
+        {
+            return "Unblocked access";
+        }
+        [ServiceFilter(typeof(IpBlockActionFilter))]
+        [HttpGet("blocked")]
+        public string blocked()
+        {
+            return "blocked access";
+        }
+
+    }
+}
