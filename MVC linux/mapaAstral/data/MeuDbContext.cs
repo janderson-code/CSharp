9cf8d5e493eb309e697ca1c8746be60fbddb6d91
Janderson  Barbosa <janderson.goncalves@bkbank.com.br>
2023-04-24 21:45:44 -0300

    Update

--- MVC linux/mapaAstral/data/MeuDbContext.cs
+++ MVC linux/mapaAstral/data/MeuDbContext.cs
@@ -11,26 +11,30 @@ namespace mapaAstral.data
 
     public class MeuDbContext : DbContext
     {
+        public DbSet<Planeta> Planetas { get; set; }
+        public DbSet<Signo> Signos { get; set; }
+
         public MeuDbContext(DbContextOptions<MeuDbContext> options)
             : base(options)
         {
+            Signos = Set<Signo>();
+            Planetas = Set<Planeta>();
         }
-
-        public DbSet<Signo> Signos { get; set; }
-        public DbSet<Planeta> Planetas { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Signo>().ToTable("Signos");
             modelBuilder.Entity<Planeta>().ToTable("Planetas");
         }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=mapa_astral_db;Username=postgres;Password=kasama");
+                optionsBuilder.UseNpgsql(
+                    "Host=localhost;Port=5432;Database=mapa_astral_db;Username=postgres;Password=kasama"
+                );
             }
         }
     }
-
 }
