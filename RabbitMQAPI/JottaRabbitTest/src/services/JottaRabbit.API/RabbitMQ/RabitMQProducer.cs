b6050bd417e8c27c0e2b80474bf30e64c118c182
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2023-05-03 17:33:14 -0300

    Update RabitMQProducer.cs

--- RabbitMQAPI/JottaRabbitTest/src/services/JottaRabbit.API/RabbitMQ/RabitMQProducer.cs
+++ RabbitMQAPI/JottaRabbitTest/src/services/JottaRabbit.API/RabbitMQ/RabitMQProducer.cs
@@ -18,14 +18,17 @@ namespace JottaRabbit.API.RabbitMQ
             };
             //Create the RabbitMQ connection using connection factory details as i mentioned above
             var connection = factory.CreateConnection();
+            
             //Here we create channel with session and model
             using var channel = connection.CreateModel();
-            //declare the queue after mentioning name and a few property related to that
 
+            //declare the queue after mentioning name and a few property related to that
             channel.QueueDeclare("product", exclusive: false);
+
             //Serialize the message
             var json = JsonConvert.SerializeObject(message);
             var body = Encoding.UTF8.GetBytes(json);
+
             //put the data on to the product queue
             channel.BasicPublish(exchange: "", routingKey: "product", body: body);
         }
