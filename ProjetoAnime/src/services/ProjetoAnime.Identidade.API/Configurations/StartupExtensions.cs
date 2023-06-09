86da5535f5b74161daaeb430409e77a208f98a69
Janderson  Barbosa <janderson.goncalves@bkbank.com.br>
2023-06-09 19:27:39 -0300

    Update

---
+++ ProjetoAnime/src/services/ProjetoAnime.Identidade.API/Configurations/StartupExtensions.cs
@@ -0,0 +1,19 @@
+namespace ProjetoAnime.Identidade.API.Configurations;
+
+public static class StartupExtensions
+{
+    public static WebApplicationBuilder UseStartup<TStartup>(this WebApplicationBuilder webApplicationBuilder) where TStartup : IStartup
+    {
+        var startup = Activator.CreateInstance(typeof(TStartup), webApplicationBuilder.Configuration) as IStartup;
+
+        if (startup == null) throw new ArgumentException("Classe startup invalida");
+
+        startup.ConfigureService(webApplicationBuilder.Services);
+
+        var app = webApplicationBuilder.Build();
+        startup.Configure(app, app.Environment);
+        app.Run();
+
+        return webApplicationBuilder;
+    }
+}
---
+++ ProjetoAnime/src/services/ProjetoAnime.Manga.API/Configuration/Startup.cs
@@ -0,0 +1,35 @@
+namespace ProjetoAnime.Manga.API.Configuration;
+
+public class Startup : IStartup
+{
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void Configure(WebApplication app, IWebHostEnvironment env)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+    }
+
+    public void ConfigureService(IServiceCollection services)
+    {
+        services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+}
