c08066d950c5885923184232388421f8cea37e0a
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2023-06-23 14:15:53 -0300

    Update

---
+++ ProjetoAnime/Architecture.Test/AchictectureTests.cs
@@ -0,0 +1,36 @@
+
+namespace Architecture.Test;
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test(Description = "Teste do Assembly do Core")]
+    public void Core_Should_Not_haveDependencyOnOtherProjects()
+    {
+        // Arrange
+        var assembly = typeof(CoreAssembly).Assembly;
+
+        Type type = typeof(ArchitectureTestsNameSpace);
+
+        var properties = type
+            .GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static)
+            .Where(p => p.FieldType == typeof(string))
+            .Select(p => p.GetValue(null) as string).ToArray();
+
+        // Act
+        var testResult = Types
+                     .InAssembly(assembly)
+                     .ShouldNot()
+                     .HaveDependencyOnAll(properties)
+                     .GetResult();
+
+        // Assert
+
+        var result = testResult.IsSuccessful;
+
+        Assert.IsTrue(result);
+    }
+}
---
+++ ProjetoAnime/Architecture.Test/ArchitectureTestsNameSpace.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Architecture.Test;
+public class ArchitectureTestsNameSpace
+{
+    private const string DomainNameSpace = "Domain";
+    private const string ApplicationNamespace = "Application";
+    private const string InfrastructureNamespace = "Infrastructure";
+    private const string PresentationNamespace = "Presentation";
+    private const string WebNamespace = "ProjetoAnime.Webapp.MVC";
+    private const string CoreNamespace = "ProjetoAnime.KitsuGateway.Core";
+    private const string ApiCoreNamespace = "ProjetoAnime.Core.API";
+    private const string KitsuGatewayNamespace = "ProjetoAnime.KitsuGateway.API";
+}
+
