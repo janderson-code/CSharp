52641cb2358328845714be033d3930c56b6cd30c
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2023-06-28 16:18:31 -0300

    Adicionado classe de serviço para tradução

---
+++ ProjetoAnime/src/web/ProjetoAnime.Webapp.MVC/Services/ITradutorService.cs
@@ -0,0 +1,7 @@
+﻿namespace ProjetoAnime.Webapp.MVC.Services
+{
+    public interface ITradutorService
+    {
+        public Task<string> TranslateText(string text, string targetLanguage);
+    }
+}
