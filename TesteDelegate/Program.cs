fdceff99d770553560e30e32065aeef557bb5328
Janderson Goncalves <janderson.goncalves@bkbank.com.br>
2023-07-31 10:44:19 -0300

    Updated

--- TesteDelegate/Program.cs
+++ TesteDelegate/Program.cs
@@ -1,108 +1,112 @@
 ﻿using System;
 namespace TesteDelegate
 {
+    //Example 1
+    delegate List<int> MeuDelegate(int x, int y);
 
-//Example 1
-delegate List<int> MeuDelegate(int x, int y);
+    // Example 2
+    public delegate int del(int x);
+    public delegate void Deltexto(string texto);
 
-// Example 2
-public delegate int del (int x);
-public  delegate void Deltexto(string texto);
+    //Example 3
+    public delegate int DelegateCalc(int num1, int num2);
 
-
-//Example 3
-public delegate int DelegateCalc(int num1, int num2);  
-
-
-public class Program
-{
-    public static void Main(string[] args)
+    public class Program
     {
-        //Example 1
-        MeuDelegate meuDelegate = new MeuDelegate(Soma);
-        meuDelegate += Subtracao;
-        meuDelegate += Multiplicacao;
-
-        List<int> resultados = meuDelegate(2, 3);
-
-        foreach (int resultado in resultados)
+        public static void Main(string[] args)
         {
-            Console.WriteLine($"Resultado => {resultado}");
+            //Example 1
+            MeuDelegate meuDelegate = new MeuDelegate(Soma);
+            meuDelegate += Subtracao;
+            meuDelegate += Multiplicacao;
+
+            List<int> resultados = meuDelegate(2, 3);
+
+            foreach (int resultado in resultados)
+            {
+                Console.WriteLine($"Resultado => {resultado}");
+            }
+
+            List<Delegate> delegates = meuDelegate.GetInvocationList().ToList();
+
+            foreach (Delegate del in delegates)
+            {
+                MeuDelegate metodo = (MeuDelegate)del;
+                List<int> resultadoMetodo = metodo(2, 3);
+                Console.WriteLine(resultadoMetodo[0]);
+            }
+
+            // Example 2
+            del handle = testeDel;
+            handle += testeDel2;
+            handle += (a) => 2 * 10;
+            Console.WriteLine(handle(3));
+            Deltexto handleTexto = textoTeste;
+            MetodoCallback(1, 2, handleTexto);
+
+            System.Console.WriteLine("///App");
+            ChatApp chatApp = new ChatApp();
+
+            Usuario usuario1 = new Usuario("Alice");
+            Usuario usuario2 = new Usuario("Bob");
+
+            // Registrar os callbacks dos usuários para receber mensagens
+            chatApp.NovaMensagem += usuario1.ReceberMensagem;
+            chatApp.NovaMensagem += usuario2.ReceberMensagem;
+
+            chatApp.EnviarMensagem("Carol", "Olá, pessoal!");
+            chatApp.EnviarMensagem("janderson", "Olá, pessoal!");
+
+            //Example3
+
+            Calculadora calc = new Calculadora();
+            DelegateCalc delegateTeste = new DelegateCalc(calc.Add(1, 2));
         }
 
-        List<Delegate> delegates = meuDelegate.GetInvocationList().ToList();
-        
-        foreach (Delegate del in delegates)
+        // Example1
+        static List<int> Soma(int x, int y)
         {
-            MeuDelegate metodo = (MeuDelegate) del;
-            List<int> resultadoMetodo = metodo(2, 3);
-            Console.WriteLine(resultadoMetodo[0]);
+            List<int> resultados = new List<int>();
+            int resultado = x + y;
+            resultados.Add(resultado);
+            return resultados;
         }
 
-        // Example 2
-        del handle = testeDel;
-		handle += testeDel2;
-		handle +=(a) => 2 *10;
-		Console.WriteLine(handle(3));
-		Deltexto handleTexto = textoTeste;
-		MetodoCallback(1,2,handleTexto);
+        static List<int> Subtracao(int x, int y)
+        {
+            List<int> resultados = new List<int>();
+            int resultado = x - y;
+            resultados.Add(resultado);
+            return resultados;
+        }
 
-        System.Console.WriteLine("///App");
-        ChatApp chatApp = new ChatApp();
+        static List<int> Multiplicacao(int x, int y)
+        {
+            List<int> resultados = new List<int>();
+            int resultado = x * y;
+            resultados.Add(resultado);
+            return resultados;
+        }
+        public static int testeDel(int num)
+        {
+            return num * 2;
+        }
 
-        Usuario usuario1 = new Usuario("Alice");
-        Usuario usuario2 = new Usuario("Bob");
-
-        // Registrar os callbacks dos usuários para receber mensagens
-        chatApp.NovaMensagem += usuario1.ReceberMensagem;
-        chatApp.NovaMensagem += usuario2.ReceberMensagem;
-
-        chatApp.EnviarMensagem("Carol", "Olá, pessoal!");
-        chatApp.EnviarMensagem("janderson", "Olá, pessoal!");
+        // Example2
+        public static int testeDel2(int num)
+        {
+            return num * 3;
+        }
+        public static void textoTeste(string message)
+        {
+            Console.Write(message);
+        }
+        public static void MetodoCallback(int num, int num2, Deltexto callback)
+        {
+            callback("O calculo é" + (num + num2).ToString());
+        }
     }
-
-    static List<int> Soma(int x, int y)
-    {
-        List<int> resultados = new List<int>();
-        int resultado = x + y;
-        resultados.Add(resultado);
-        return resultados;
-    }
-
-    static List<int> Subtracao(int x, int y)
-    {
-        List<int> resultados = new List<int>();
-        int resultado = x - y;
-        resultados.Add(resultado);
-        return resultados;
-    }
-
-    static List<int> Multiplicacao(int x, int y)
-    {
-        List<int> resultados = new List<int>();
-        int resultado = x * y;
-        resultados.Add(resultado);
-        return resultados;
-    }
-    	public static int testeDel(int num){
-		
-		return num * 2;
-	}
-	public static int testeDel2(int num){
-	
-		return num *3;
-	}
-	public static void textoTeste(string message){
-	
-		Console.Write(message);
-	}
-	public static void MetodoCallback(int num, int num2, Deltexto callback)
-	{
-	   callback("O calculo é"+ (num + num2).ToString());
-	}
 }
-}
-
 
 // Delegate para notificar os usuários sobre uma nova mensagem
 delegate void NovaMensagemDelegate(string remetente, string mensagem);
@@ -138,16 +142,17 @@ class Usuario
     }
 }
 
+// Example3
+
 public class Calculadora
 {
     public int Add(int num, int num2)
     {
-        return num +  num2;
+        return num + num2;
     }
 
     public int Multiply(int num, int num2)
     {
         return num * num2;
     }
-
 }
--- TesteDelegate/TesteAction.cs
+++ TesteDelegate/TesteAction.cs
@@ -3,58 +3,52 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
-					
 public class Teste
 {
-	public static void Main()
-	{
-	 /*	var lista = new[]{"Corinthians","Santos","Palmeiras"};
-		Action<string> minhaAction = new Action<string>(Console.WriteLine);
-		
-		//Array.ForEach(lista,Console.WriteLine);
-		
-		Array.ForEach(lista,minhaAction);
-		*/
-		Clube cl = new Clube();
-		
-		var listaTimes = cl.ListarTimes();
-		
-		Action<Clube> actionClube = new Action<Clube>(cl.MudarNomeClube);
-		
-		listaTimes.ForEach(actionClube);
-		
-		foreach(var clube in listaTimes){
-		
-			Console.WriteLine(clube.Nome);
-		}
-	}
-	
+    public static void Main()
+    {
+        /*	var lista = new[]{"Corinthians","Santos","Palmeiras"};
+           Action<string> minhaAction = new Action<string>(Console.WriteLine);
+
+           //Array.ForEach(lista,Console.WriteLine);
+
+           Array.ForEach(lista,minhaAction);
+           */
+        Clube cl = new Clube();
+
+        var listaTimes = cl.ListarTimes();
+
+        Action<Clube> actionClube = new Action<Clube>(cl.MudarNomeClube);
+
+        listaTimes.ForEach(actionClube);
+
+        foreach (var clube in listaTimes)
+        {
+            Console.WriteLine(clube.Nome);
+        }
+    }
 }
 
-public class Clube{
+public class Clube
+{
+    public string Nome { get; set; }
+    public string Estadio { get; set; }
 
-	public string Nome{get;set;}
-	public string Estadio{get;set;}
+    public List<Clube> ListarTimes()
+    {
+        return new List<Clube>() { new Clube { Nome = "Corinthians", Estadio = "Itaquera" }, new Clube { Nome = "Real madrid", Estadio = "Bernabeu" } };
+    }
 
-
-	public List<Clube> ListarTimes()
-	{
-
-		return new List<Clube>(){new Clube{Nome ="Corinthians",Estadio= "Itaquera"},new Clube{Nome = "Real madrid",Estadio="Bernabeu"}};
-	}
-	
-	public void MudarNomeClube(Clube clube){
-	
-		clube.Nome = "Barcelona";
-	}
+    public void MudarNomeClube(Clube clube)
+    {
+        clube.Nome = "Barcelona";
+    }
 }
 
 public class TesteAction
 {
-
     public void Teste()
     {
-
         List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
 
         // Declaração de variáveis para armazenar os resultados.
