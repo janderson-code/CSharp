d9f07d8e44a9c99dfa9379ca700534e07192208f
janderson-code <jbgfilebarbosa@gmail.com>
2023-10-29 22:16:00 -0300

    save

---
+++ LINQ/TesteLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.AccessControl;
+using System.Linq;
+
+List<int> num = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+var env = num.Where(x => x % 2 == 0);
+
+num.Add(10);
+
+System.Console.WriteLine(string.Join(",", env));
+
+Console.ReadLine();
